c69e795133e0d365a6b3a5d41822ec1f1f9a4fe9
Udit Agarwal <hello@uditagarwal.com>
2016-10-20 02:11:41 +0530

    Remove test added for debugging purposes

--- tests/TwitterClientTests.php
+++ tests/TwitterClientTests.php
@@ -5,18 +5,6 @@ use PHPUnit\Framework\TestCase;
 require_once '../TwitterClient.php';
 
 class TwitterClientTests extends TestCase {
-    public function testPushAndPop() {
-        $stack = [];
-        $this->assertEquals(0, count($stack));
-
-        array_push($stack, 'foo');
-        $this->assertEquals('foo', $stack[count($stack)-1]);
-        $this->assertEquals(1, count($stack));
-
-        $this->assertEquals('foo', array_pop($stack));
-        $this->assertEquals(0, count($stack));
-    }
-    
     public function testItReturnsOnlyAtleastOnceRetweetedTweets() {
         $stub = $this->createMock(Abraham\TwitterOAuth\TwitterOAuth::class);
         $stub->method('get')
