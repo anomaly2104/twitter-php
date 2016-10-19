17120a27a372e5d693d7be753ff033fd792481a6
Udit Agarwal <hello@uditagarwal.com>
2016-10-20 03:20:47 +0530

    Remove debug logs

--- media/js/index.js
+++ media/js/index.js
@@ -5,30 +5,20 @@ $(document).ready(function () {
     
     function fetchTweets(callback) {
         $.getJSON("./api/getTweets.php", function (tweets) {
-            console.log("tweets fetched");
-            console.log(tweets);
             twttr.ready(function () {
-                console.log("twtrr loaded");
                 callback(tweets);
             });
         });
     }
     
     function renderTweets(tweets) {
-        console.log("rendering  tweets: " + tweets);
-        
-        console.log(tweets);
-        
-        for (var i = 0; i < tweets.length; ++i) {
+       for (var i = 0; i < tweets.length; ++i) {
             var tweet = tweets[i];
             renderTweet(tweet);
         }
     }
     
     function renderTweet(tweet) {
-        console.log("rendering  tweet: ");
-        console.log(tweet);
-          
         $('#tweets').append("<div id='" + tweet.id + "' class='col-md-4 col-lg-4'></div>");
         twttr.widgets.createTweet(tweet.id, document.getElementById(tweet.id), {
           cards: 'hidden',
