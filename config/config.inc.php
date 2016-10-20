ed85936232407be091d8f81eaecf9dafd1a7ef81
Udit Agarwal <hello@uditagarwal.com>
2016-10-20 21:22:15 +0530

    Gitignore config file

--- config/config.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('CONSUMER_KEY', '6CGPiuK35QztjEqURsDqlyV7o');
-define('CONSUMER_SECRET', 'cadXC8l92EcPNwaZbR13tdKTHkE37zRof7c7nga5OnSpg9fOoe');
-
-define('OAUTH_ACCESS_TOKEN', '96310407-j1maqxzPqso2uC5OYyqKwPiyibgc5OvNm7jFKnH4F');
-define('OAUTH_ACCESS_TOKEN_SECRET', 'qpvMlfxoNf4K5AALvhhlsWTV5Y3gTZkiofrioDnSwDZLr');
